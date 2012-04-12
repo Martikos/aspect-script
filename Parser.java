714aed4da3147df9344cd309410fd4524c88a9fa
Marc Adam <marc.m.adam@gmail.com>
2012-04-12 13:56:05 +0300

    ignoring empty lines

--- Parser.java
+++ Parser.java
@@ -19,17 +19,25 @@ public class Parser {
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
         String strLine;
     
-        // first, read whole file to get title and all env variables
         while((strLine = br.readLine())!=null) {
             System.out.println(strLine);
             String[] strs = strLine.split(" ");
             if (strLine.startsWith("title")) {
+                // get class name and title in line
                 title = strs[1];
             }
             else if (strLine.startsWith("env")) {
+                // get environment variable in line
                 String variableName = strs[1];
                 String variableValue = strs[2];
                 envVariables.put(variableName, variableValue);
+            }
+            else if (strLine.equals("")) {
+                // ignore
+            } 
+            else {
+                // actual processing 
+
             }
         }
     }
